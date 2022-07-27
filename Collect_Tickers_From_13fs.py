32356171cf3c4c3af76203f3dd2db98d3c8b229f
BlakeMLoyola <109235721+BlakeMLoyola@users.noreply.github.com>
2022-07-27 11:41:46 -0500

    Update Collect_Tickers_From_13fs.py

--- Collect_Tickers_From_13fs.py
+++ Collect_Tickers_From_13fs.py
@@ -1,33 +1,137 @@
-""" this program was designed to collect tickers from CUSIPS using a fidelity
-search engine. It will not be able to collect tickers from all 13F entries given
-that CUSIPS and public stocks change a lot. It works best on recent 13F filings
-but is also useful to get started on for older filings. First check Whale Wisdom to
-see if the 13F is available on there, with tickers. Lot easier that way."""
-
-import os
-from bs4 import BeautifulSoup
-import matplotlib.pyplot as plt
+from unicodedata import name
 import pandas as pd
 import requests
+import re
+import csv
+import lxml
+from bs4 import BeautifulSoup
 import time
+import yfinance as yf
+
+sec_url = 'https://www.sec.gov'
 
 
-""" In order to run the program, 13fs must first be stored within a CSV file,
-and the CUSIP Column must be called "CUSIP." See "Example_13F.csv" as a guide
-for how I structured my CSV""""
+# Defining request
+def get_request(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'HOST': 'www.sec.gov',
+    }
+    return requests.get(url, headers=headers)
 
-files = pd.read_csv("ENTER_NAME.csv")
-files = pd.DataFrame(files)
+
+#URL for EDGAR
+def create_url(cik):
+    return 'https://www.sec.gov/cgi-bin/browse-edgar?CIK={}&owner=exclude&action=getcompany&type=13F-HR'.format(cik)
 
 
-cusip_nums = set()
-for file in files:
-    cusip_nums = files.CUSIP
+# Input of CIK by User
+def get_user_input():
+    cik = input("Enter 10-digit CIK number: ")
+    return cik
+requested_cik = get_user_input()
+
+def get_quarters():
+    years = int(input("Please enter the number of years you would like to pull ('2' would pull previous 8 quarters):"))
+    quarter = 4
+    quarters = int(years*quarter)
+    return quarters
+
+
+                  
+
+
+#Scraping the company report
+def scrap_company_report(requested_cik):
+    # Find mutual fund by CIK number on EDGAR
+    response = get_request(create_url(requested_cik))
+    soup = BeautifulSoup(response.text, "html.parser")
+    tags = soup.findAll('a', id="documentsbutton")
+
+    last_report = (sec_url + tags[0]['href'])
+    previous_report = (sec_url + tags[1]['href'])
+
     
-#Creating Ticker Dictionary
-ticker_dic = {c:"" for c in cusip_nums}
+    scrap_report_by_url(last_report, "last_report")
+    scrap_report_by_url(previous_report, "previous_report")
+    
+  
+        
 
-#Running all cusips within ticker dictionary through the fidelity engine
+
+def scrap_report_by_url(url, filename):
+    response_two = get_request(url)
+    soup_two = BeautifulSoup(response_two.text, "html.parser")
+    tags_two = soup_two.findAll('a', attrs={'href': re.compile('xml')})
+    xml_url = tags_two[3].get('href')
+
+    response_xml = get_request(sec_url + xml_url)
+    soup_xml = BeautifulSoup(response_xml.content, "lxml")
+    xml_to_csv(soup_xml, filename)
+
+#Creating CSV out of XML
+def xml_to_csv(soup_xml, name):
+
+    columns = [
+        "Name of Issuer",
+        "CUSIP",
+        "Value (x$1000)",
+        "Shares",
+        "Investment Discretion",
+        "Voting Sole / Shared / None"
+    ]
+    issuers = soup_xml.body.findAll(re.compile('nameofissuer'))
+    cusips = soup_xml.body.findAll(re.compile('cusip'))
+    values = soup_xml.body.findAll(re.compile('value'))
+    sshprnamts = soup_xml.body.findAll('sshprnamt')
+    sshprnamttypes = soup_xml.body.findAll(re.compile('sshprnamttype'))
+    investmentdiscretions = soup_xml.body.findAll(re.compile('investmentdiscretion'))
+    soles = soup_xml.body.findAll(re.compile('sole'))
+    shareds = soup_xml.body.findAll(re.compile('shared'))
+    nones = soup_xml.body.findAll(re.compile('none'))
+
+    df = pd.DataFrame(columns= columns)
+
+    for issuer, cusip, value, sshprnamt, sshprnamttype, investmentdiscretion, sole, shared, none in zip(issuers, cusips, values, sshprnamts, sshprnamttypes, investmentdiscretions, soles, shareds, nones):
+        row = {
+            "Name of Issuer": issuer.text,
+            "CUSIP": cusip.text,
+            "Value (x$1000)": value.text,
+            "Shares": f"{sshprnamt.text} {sshprnamttype.text}",
+            "Investment Discretion": investmentdiscretion.text,
+            "Voting Sole / Shared / None": f"{sole.text} / {shared.text} / {none.text}"
+        }
+        df = df.append(row, ignore_index=True)
+
+    df.to_csv(f"{name}.csv")
+
+
+    
+
+
+
+
+scrap_company_report(requested_cik)
+
+
+#Generating CSV of last and previous report
+df1 = pd.DataFrame(pd.read_csv("last_report.csv"))
+df2 = pd.DataFrame(pd.read_csv("previous_report.csv"))
+
+#Concat the two Dfs
+dfs = [df1,df2]
+result = pd.concat(dfs)
+
+
+
+
+#Using fidelity search engine to return tickers for holdings, some may not return ticker if CUSIP has changed
+cusip_nums = set()
+for row in dfs:
+    cusip_nums = result.CUSIP
+
+ticker_dic = {c:"" for c in cusip_nums}
 for c in list(ticker_dic.keys()):
     url = "http://quotes.fidelity.com/mmnet/SymLookup.phtml?reqforlookup=REQUESTFORLOOKUP&productid=mmnet&isLoggedIn=mmnet&rows=50&for=stock&by=cusip&criteria="+c+"&submit=Search"
     html = requests.get(url).text
@@ -41,6 +145,16 @@ for c in list(ticker_dic.keys()):
         pass
 
     time.sleep(1)
+    
+
+#mapping results
+result['Ticker'] = result['CUSIP'].map(ticker_dic)
+    
+  
+
+
+result.to_csv("Output.csv")
+
     
 # Mapping to dataframe
 files['Ticker'] = files['CUSIP'].map(ticker_dic)
